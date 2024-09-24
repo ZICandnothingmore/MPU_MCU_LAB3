139edd93ae1778ba365d6984b7927bf9c5e9db79
ZICandnothingmore <baonhu0508@gmail.com>
2024-09-24 14:13:16 +0700

    first commit

---
+++ Source/Core/Inc/Ex5.h
@@ -0,0 +1,17 @@
+/*
+ * Ex5.h
+ *
+ *  Created on: 24 thg 9, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EX5_H_
+#define INC_EX5_H_
+
+#include "main.h"
+#include "displayTrafficLight.h"
+
+void ex5_init();
+void ex5_run();
+
+#endif /* INC_EX5_H_ */
---
+++ Source/Core/Src/Ex5.c
@@ -0,0 +1,16 @@
+/*
+ * Ex5.c
+ *
+ *  Created on: 24 thg 9, 2024
+ *      Author: ACER
+ */
+
+#include "Ex5.h"
+
+void ex5_init(){
+
+}
+
+void ex5_run(){
+
+}
